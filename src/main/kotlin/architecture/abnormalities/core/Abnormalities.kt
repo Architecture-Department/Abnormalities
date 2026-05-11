045f324895098fd406017ee612a56c9761661dcc
王小尽 <1809315974@qq.com>
2026-05-11 16:45:36 +0800

    继续迁移到kt

---
+++ src/main/kotlin/architecture/abnormalities/core/Abnormalities.kt
@@ -0,0 +1,68 @@
+package architecture.abnormalities.core
+
+import architecture.abnormalities.init.AbnormalitiesCreativeModeTabs
+import architecture.abnormalities.init.AbnormalitiesEntityDataSerializers
+import architecture.abnormalities.init.AbnormalitiesSoundEvents
+import architecture.abnormalities.init.AbnormalitiesSpawnEggItems
+import architecture.abnormalities.init.entity.AbnormalitiesEntityTypes
+import net.minecraft.core.Registry
+import net.minecraft.resources.ResourceKey
+import net.minecraft.resources.ResourceLocation
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.common.Mod
+import net.neoforged.neoforge.event.server.ServerStartingEvent
+import net.neoforged.neoforge.registries.DeferredRegister
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.jetbrains.annotations.Contract
+import thedarkcolour.kotlinforforge.neoforge.forge.LOADING_CONTEXT
+import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
+
+@Mod(Abnormalities.ID)
+@EventBusSubscriber
+object Abnormalities {
+	const val ID: String = "abnormalities"
+	const val NAME: String = "Abnormalities"
+
+	@JvmField
+	val LOGGER: Logger = LogManager.getLogger(ID)
+
+	init {
+		val modContainer = LOADING_CONTEXT.activeContainer
+		val modBus = MOD_BUS
+
+		AbnormalitiesEntityDataSerializers.REGISTRY.register(modBus)
+		AbnormalitiesCreativeModeTabs.REGISTRY.register(modBus)
+		AbnormalitiesSoundEvents.REGISTRY.register(modBus)
+		AbnormalitiesEntityTypes.init(modBus)
+		AbnormalitiesSpawnEggItems.REGISTRY.register(modBus)
+	}
+
+	@SubscribeEvent
+	fun onServerStarting(event: ServerStartingEvent) {
+		LOGGER.info("HELLO from server starting")
+	}
+
+	@JvmStatic
+	@Contract("_ -> new")
+	fun modRl(name: String): ResourceLocation {
+		return ResourceLocation.fromNamespaceAndPath(ID, name)
+	}
+
+	@JvmStatic
+	@Contract(pure = true)
+	fun modRlText(name: String): String {
+		return "$ID:$name"
+	}
+
+	@JvmStatic
+	fun <T> modRegister(registry: Registry<T>): DeferredRegister<T> {
+		return DeferredRegister.create<T>(registry, ID)
+	}
+
+	@JvmStatic
+	fun <T> modRegister(registry: ResourceKey<Registry<T>>): DeferredRegister<T> {
+		return DeferredRegister.create<T>(registry, ID)
+	}
+}
---
+++ src/main/kotlin/architecture/abnormalities/core/AbnormalitiesClient.kt
@@ -0,0 +1,32 @@
+package architecture.abnormalities.core
+
+import net.minecraft.client.Minecraft
+import net.neoforged.api.distmarker.Dist
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.common.Mod
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
+import net.neoforged.neoforge.client.gui.ConfigurationScreen
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory
+import thedarkcolour.kotlinforforge.neoforge.forge.LOADING_CONTEXT
+import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
+
+@Mod(value = Abnormalities.ID, dist = [Dist.CLIENT])
+@EventBusSubscriber(modid = Abnormalities.ID, value = [Dist.CLIENT])
+object AbnormalitiesClient {
+	init {
+		val modContainer = LOADING_CONTEXT.activeContainer
+		val modBus = MOD_BUS
+
+		modContainer.registerExtensionPoint(
+			IConfigScreenFactory::class.java,
+			IConfigScreenFactory(::ConfigurationScreen)
+		)
+	}
+
+	@SubscribeEvent
+	fun onClientSetup(event: FMLClientSetupEvent) {
+		Abnormalities.LOGGER.info("HELLO FROM CLIENT SETUP")
+		Abnormalities.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().user.name)
+	}
+}
