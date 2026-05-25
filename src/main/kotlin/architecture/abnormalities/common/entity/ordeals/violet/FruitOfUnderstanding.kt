91f483beb267972dd73258b2864159b2479cd7e9
王小尽 <1809315974@qq.com>
2026-05-25 13:06:08 +0800

    修复初始化问题和生物代码问题

--- src/main/kotlin/architecture/abnormalities/common/entity/ordeals/violet/FruitOfUnderstanding.kt
+++ src/main/kotlin/architecture/abnormalities/common/entity/ordeals/violet/FruitOfUnderstanding.kt
@@ -12,7 +12,6 @@ import net.minecraft.network.syncher.EntityDataAccessor
 import net.minecraft.network.syncher.EntityDataSerializers
 import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.world.damagesource.DamageSource
-import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.PathfinderMob
@@ -146,10 +145,6 @@ class FruitOfUnderstanding(entityType: EntityType<out PathfinderMob>, level: Lev
 			target != null &&
 			target.isAlive &&
 			!isCamp(target)
-	}
-
-	override fun canTarget(entity: Entity): Boolean {
-		return (this as IOrdealsVioletEntity).canTarget(entity)
 	}
 
 	private fun tryReduceSelfDestructCounter() {
---
+++ src/main/kotlin/architecture/abnormalities/core/AbnormalitiesConstants.kt
@@ -0,0 +1,40 @@
+package architecture.abnormalities.core
+
+import architecture.goldenboughs_lib.util.LibUtil.rlOf
+import net.minecraft.core.Registry
+import net.minecraft.resources.ResourceKey
+import net.minecraft.resources.ResourceLocation
+import net.neoforged.neoforge.registries.DeferredRegister
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.jetbrains.annotations.Contract
+
+object AbnormalitiesConstants {
+	const val ID: String = "abnormalities"
+	const val NAME: String = "Abnormalities"
+
+	@JvmField
+	val LOGGER: Logger = LogManager.getLogger(ID)
+
+	@JvmStatic
+	@Contract("_ -> new")
+	fun modRl(name: String): ResourceLocation {
+		return rlOf(ID, name)
+	}
+
+	@JvmStatic
+	@Contract(pure = true)
+	fun modRlText(name: String): String {
+		return "$ID:$name"
+	}
+
+	@JvmStatic
+	fun <T> modRegister(registry: Registry<T>): DeferredRegister<T> {
+		return DeferredRegister.create<T>(registry, ID)
+	}
+
+	@JvmStatic
+	fun <T> modRegister(registry: ResourceKey<Registry<T>>): DeferredRegister<T> {
+		return DeferredRegister.create<T>(registry, ID)
+	}
+}
